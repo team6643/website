cd324e6e7e21fffdf8e62d2b8d58544094abdc88
Walnuts and Bolts <210421357+team6643@users.noreply.github.com>
2025-11-14 15:26:36 -0600

    Update About.tsx

--- client/src/routes/About.tsx
+++ client/src/routes/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
         
             <div class="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 gap-2.5 py-8 min-h-screen px-5">
                 <div class="flex flex-col items-center max-w-5xl text-center gap-4">
-                    <h1 class="text-4xl font-bold">About us</h1>
+                    <h1 class="text-4xl font-bold">About Us</h1>
                     <p>
                         Team 6643 was founded in 2016 at Walden III High School in Racine, Wisconsin. We strive to provide students with opportunities to grow in their knowledge and understanding of science, technology, engineering, mathematics, business, leadership, and marketing. We also promote growth both as individuals and as a team. While we are competitive, we also believe in being gracious professionals at school, in our community, and at competitions. What's more, our team is completely youth-run.
                     </p>
