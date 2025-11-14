69284a2275f9c7a199eec73bacdebc11dc7bb1c1
Walnuts and Bolts <210421357+team6643@users.noreply.github.com>
2025-11-14 15:47:05 -0600

    Update PhotoGallery.tsx

--- client/src/routes/PhotoGallery.tsx
+++ client/src/routes/PhotoGallery.tsx
@@ -3,7 +3,7 @@ const PhotoGallery = () => {
 
         <div class="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 gap-2.5 py-8 min-h-screen px-5">
         <div class="flex flex-col items-center max-w-5xl text-center gap-4">
-            <h1 class="text-4xl font-bold">Photos!!!</h1>
+            <h1 class="text-4xl font-bold">Photos</h1>
 
             <p>
                 
