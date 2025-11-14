dffae23c1e9186d2de8c1f70a095507d50bd5dc6
Walnuts and Bolts <210421357+team6643@users.noreply.github.com>
2025-11-14 15:40:37 -0600

    Update PhotoGallery.tsx

--- client/src/routes/PhotoGallery.tsx
+++ client/src/routes/PhotoGallery.tsx
@@ -1,1 +1,18 @@
+const PhotoGallery = () => {
+    return (
 
+        <div class="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 gap-2.5 py-8 min-h-screen px-5">
+        <div class="flex flex-col items-center max-w-5xl text-center gap-4">
+            <h1 class="text-4xl font-bold">Photos!!!</h1>
+
+            <p>
+                
+            Here are some of our many team photos
+            
+        </div>
+          
+        </div>
+    );
+}
+
+export default PhotoGallery;
