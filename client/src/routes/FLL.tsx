dfbab93edaf84adf147a3d2f36841f7204813a75
Theo Matter <theomatter2@gmail.com>
2026-01-29 11:56:12 -0600

    Update FLL.tsx

--- client/src/routes/FLL.tsx
+++ client/src/routes/FLL.tsx
@@ -7,9 +7,9 @@ const FLL = () => {
                 <h1 class="text-4xl font-bold">FLL</h1>
                 <p>
                 The FIRST Lego League Challenge (FLL) is a FIRST program for students ages 9-14. In this program, students are given an "innovation project" prompt that involves the solving of a common issue or obstacle in the community. Students work together to research, plan, and innovate a solution to this real-world problem, which is later presented to a panel of judges. 
-
+                /n
                 In addition, students are also required to design, build, and program robots using a standard robot set. This robot is then set inside a "gameplay" mat which contains Lego-built missions that the robots are programmed to complete for scoring points. 
-
+                /n
                 Participating in this program grants students exposure to practical, real-world STEM applications and cooperative skills.
                 </p>
             </div>
