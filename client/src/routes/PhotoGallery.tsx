44de0a5912b8537de99354442a36561052b34498
Theo Matter <theomatter2@gmail.com>
2026-01-28 19:35:15 -0600

    Update PhotoGallery.tsx

--- client/src/routes/PhotoGallery.tsx
+++ client/src/routes/PhotoGallery.tsx
@@ -4,11 +4,9 @@ const PhotoGallery = () => {
         <div class="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 gap-2.5 py-8 min-h-screen px-5">
         <div class="flex flex-col items-center max-w-5xl text-center gap-4">
             <h1 class="text-4xl font-bold">Photos</h1>
-
+            <h2 class="font-bold">Coming soon!</h2>
             <p>
-                
             Here are some of our many team photos
-
             </p>
         </div>
         </div>
