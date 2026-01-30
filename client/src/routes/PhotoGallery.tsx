5ded994e6bdefa2c2d134360fe4d8ab854b1c27c
Theo Matter <theomatter2@gmail.com>
2026-01-30 15:42:51 -0600

    Update PhotoGallery.tsx

--- client/src/routes/PhotoGallery.tsx
+++ client/src/routes/PhotoGallery.tsx
@@ -1,20 +1,19 @@
-import TeamWithAlgae from "../assets/HS Robotics 25 yearbook/20250208_163539.jpg";
-const PhotoGallery = () => {
-    return (
+import TeamWithAlgae from "../assets/HS-Robotics-25-yearbook/20250208_163539.jpg";
 
-        <div class="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 gap-2.5 py-8 min-h-screen px-5">
-        <div class="flex flex-col items-center max-w-5xl text-center gap-4">
-            <h1 class="text-4xl font-bold">Photos</h1>
-            <h2 class="font-bold">Coming soon!</h2>
-            <p>
-            Here are some of our many team photos
-            </p>
-            <div className="w-[200px] h-[200px] overflow-hidden rounded-xl">
-            <img class="object-cover" src={TeamWithAlgae} />
-            </div>
+const PhotoGallery = () => {
+  return (
+    <div className="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 gap-2.5 py-8 min-h-screen px-5">
+      <div className="flex flex-col items-center max-w-5xl text-center gap-4">
+        <h1 className="text-4xl font-bold">Photos</h1>
+        <h2 className="font-bold">Coming soon!</h2>
+        <p>Here are some of our many team photos</p>
+
+        <div className="w-[200px] h-[200px] overflow-hidden rounded-xl">
+          <img className="object-cover w-full h-full" src={TeamWithAlgae} />
         </div>
-        </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default PhotoGallery;
