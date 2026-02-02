a818687503ddecfb6dfa305c97dfd4eed8acbe64
Theo Matter <theomatter2@gmail.com>
2026-02-02 15:13:21 -0600

    Update PhotoGallery.tsx

--- client/src/routes/PhotoGallery.tsx
+++ client/src/routes/PhotoGallery.tsx
@@ -3,14 +3,14 @@ import TeamWithAlgae from "../assets/HS Robotics 25 yearbook/20250208_163539.jpg
 const PhotoGallery = () => {
     return (
 
-        <div class="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 gap-2.5 py-8 min-h-screen px-5">
-        <div class="flex flex-col items-center max-w-5xl text-center gap-4">
-            <h1 class="text-4xl font-bold">Photos</h1>
-            <h2 class="font-bold">Coming soon!</h2>
+        <div className="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 gap-2.5 py-8 min-h-screen px-5">
+        <div className="flex flex-col items-center max-w-5xl text-center gap-4">
+            <h1 className="text-4xl font-bold">Photos</h1>
+            <h2 className="font-bold">Coming soon!</h2>
             <p>
             Here are some of our many team photos
             </p>
-         <div class="w-48 h-48 overflow-hidden rounded-lg">
+         <div className="w-48 h-48 overflow-hidden rounded-lg">
           <img
             src={TeamWithAlgae}
             alt="photo"
@@ -21,3 +21,4 @@ const PhotoGallery = () => {
         </div>
     );
 }
+export default PhotoGallery;
